ee0377a09a568129bc10065a21449561231ed921
Auro <tripathy.0042@gmail.com>
2026-09-14 14:44:06 +0530

    APPSETTings

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -20,6 +20,7 @@ namespace WebApplication3.Controllers
 
         public IActionResult Privacy()
         {
+            throw new Exception("Hello");
             return View();
         }
 
--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -6,6 +6,8 @@ namespace WebApplication3
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddApplicationInsightsTelemetry();
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
